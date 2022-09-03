90c008fc859954a9bcec08df47fd6576d07513c4
Gleb Dianov <gleb.dianov@protonmail.com>
2022-09-03 15:51:11 +0200

    clock refactored

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -2,57 +2,45 @@ use std::fmt;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
+    minutes: i32,
+}
+
+struct HourClock {
     hours: i32,
     minutes: i32,
 }
 
-impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let hours_str = if self.hours.abs() < 10 {
-            format!("0{}", self.hours)
-        } else {
-            self.hours.to_string()
-        };
-
-        let minutes_str = if self.minutes.abs() < 10 {
-            format!("0{}", self.minutes)
-        } else {
-            self.minutes.to_string()
-        };
-
-        write!(f, "{}:{}", hours_str, minutes_str)
-    }
-}
+const MINUTES_IN_AN_HOUR: i32 = 60;
+const HOURS_IN_A_DAY: i32 = 24;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let div24err = "Impossible happened: dividing by 24 failed";
-        let div60err = "Impossible happened: dividing by 60 failed";
-
-        let (hours, minutes) = if minutes < 0 {
-            (
-                hours + minutes.checked_div(60).expect(div60err) - 1,
-                60 + minutes.checked_rem(60).expect(div60err),
-            )
-        } else {
-            (hours, minutes)
-        };
-
-        let hours = if hours < 0 {
-            24 + hours.checked_rem(24).expect(div24err)
-        } else {
-            hours
-        };
-
         Clock {
-            hours: (hours + minutes.checked_div(60).expect(div60err))
-                .checked_rem(24)
-                .expect(div24err),
-            minutes: minutes.checked_rem(60).expect(div60err),
+            minutes: (hours * MINUTES_IN_AN_HOUR + minutes)
+                .rem_euclid(HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock {
+            minutes: self.minutes + minutes,
+        }
+    }
+
+    fn to_hour_clock(&self) -> HourClock {
+        HourClock {
+            hours: self
+                .minutes
+                .div_euclid(MINUTES_IN_AN_HOUR)
+                .rem_euclid(HOURS_IN_A_DAY),
+            minutes: self.minutes.rem_euclid(MINUTES_IN_AN_HOUR),
+        }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hour_clock = self.to_hour_clock();
+        write!(f, "{:02}:{:02}", hour_clock.hours, hour_clock.minutes)
     }
 }
