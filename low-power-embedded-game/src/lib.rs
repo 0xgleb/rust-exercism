df1b585ce25c1bfd359fe6771cf63020fa526e0b
Gleb Dianov <gleb.dianov@protonmail.com>
2022-09-22 16:24:08 +0200

    low power embedded game exercise

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -1,25 +1,16 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    (
-        dividend
-            .checked_div(divisor)
-            .expect("Stop dividing by zero"),
-        dividend
-            .checked_rem(divisor)
-            .expect("I said stop dividing by zero"),
-    )
+    (dividend.div_euclid(divisor), dividend.rem_euclid(divisor))
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.
+    iter.enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, val)| val)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        unimplemented!("implement `fn manhattan`")
+        self.0.abs() + self.1.abs()
     }
 }
